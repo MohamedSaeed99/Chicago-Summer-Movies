2d1a9595301b65b983927013b151e6ebc36e7a52
MohamedSaeed99 <msaeed7@uic.edu>
2019-10-28 18:22:06 -0500

    changed sw

--- sw.js
+++ sw.js
@@ -11,20 +11,17 @@ var appShellFiles = [
 
 
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(appShellFiles);
-    })
-  );
+self.addEventListener('install', function(e) {
+  console.log('[Service Worker] Install');
 });
+
 
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(r) {
       console.log('[Service Worker] Fetching resource: '+ e.request.url);
       
-      return fetch(e.request).then(function(response) {
+      return r || fetch(e.request).then(function(response) {
           return caches.open(cacheName).then(function(cache) {
             console.log('[Service Worker] Caching new resource: '+e.request.url);
             cache.put(e.request, response.clone());
