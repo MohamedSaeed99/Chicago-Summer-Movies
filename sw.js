2cc4202221e05cdef636337c02e858fd66585be4
MohamedSaeed99 <msaeed7@uic.edu>
2019-10-28 21:53:14 -0500

    logging information

--- sw.js
+++ sw.js
@@ -20,6 +20,17 @@ self.addEventListener('install', function(event) {
   );
 });
 
+
+fetch('users.json').then(function(response) {
+  console.log(response.headers.get('Content-Type'));
+  console.log(response.headers.get('Date'));
+
+  console.log(response.status);
+  console.log(response.statusText);
+  console.log(response.type);
+  console.log(response.url);
+});
+
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(r) {
